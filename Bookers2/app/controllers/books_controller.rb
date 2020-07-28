b59b1b5395aca4503e587ff12dd69d67f290412d
山見駿介 <yamamishunsuke@MacBook-Pro-2.local>
2020-07-28 18:37:25 +0900

    teishutu

---
+++ Bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,60 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book)
+    else
+      @books = Book.all
+      @book = book
+      render("books/index")
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] =  "Book was successfully updated."
+      redirect_to book_path(book)
+    else
+      @book = book
+      render("books/edit")
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+  end
+
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
